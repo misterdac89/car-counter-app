8337f0edc2967d4708fe8339aa1557b7dc536b00
Louis Daccarett <89241313+misterdac89@users.noreply.github.com>
2022-12-27 23:34:21 -0500

    Add files via upload
    This is an app that helps people to count how many cars that enter a parking lot.

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-let count = 0
+let i = 0
 
+const countEl = document.getElementById("count-el")
 const incrementBtn = document.getElementById("increment-btn")
 const saveBtn = document.getElementById("save-btn")
-const countEl = document.getElementById("count-el")
 const saveEl = document.getElementById("save-el")
 
-incrementBtn.addEventListener("click", function(){
-   count++
-   countEl.textContent = count
+
+incrementBtn.addEventListener("click", function() {
+   i++
+   countEl.textContent = i
 })
 
-saveBtn.addEventListener("click", function(){
-   let countStr = count + " - "
-   saveEl.textContent += countStr
-   count = 0
+saveBtn.addEventListener("click", function() {
+   let iStr = i + " - "
+   saveEl.textContent += iStr
+   i = 0
    countEl.textContent = 0
 })
